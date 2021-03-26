9f81e22bdf316e9ecd92284449889d1c10b30271
JaiJaveria <jaijaveria16@gmail.com>
2021-03-27 02:03:41 +0530

    basic structor for first 2 instructor queries done

--- instructors.sql
+++ instructors.sql
@@ -1,5 +1,4 @@
-#add course offering
-
+#1-add course offering
 #input:
 /*
 course id
@@ -12,4 +11,33 @@ section type
 */
 insert into course_offerings values (/*course offering uuid: "new"+termcode+course id*/"",/*course id*/, /*term code*/,/*course name*/)
 insert into sections values (/*section uuid: "new"+section no+course offering*/, /* course offering uuid*/, /*section type*/, /*number*/, /*room id*/NULL, /*schedule uuid*/NULL)
+insert into teachings values (@INSTRUCTOR,@SECTION_ID)
 /*need to add the limit and the room needed somewhere*/
+/*-----------------------------------------------------------------------------*/
+
+#2-handling pending requests
+
+/*
+input
+course offering uuid
+student id
+accept or reject via variable ACCEPTED
+*/
+declare @CO text;#course_offerings
+declare @ACCEPTED boolean;
+declare @SID text;#student id
+#get all the pending requests-- a seperate query
+select * from pending_requests where course_offerings=@CO;
+#accept or reject--a separate query
+IF (@ACCEPTED)
+	begin
+	#add the student to the course. TODO
+	end
+else
+	begin
+	#put the student request with reject status
+	insert into rejected_requests values (@CO, @SID);
+	end
+#after this remove student pending request
+delete from pending_requests where course_offerings=@CO and student_id=@SID;
+/*-----------------------------------------------------------------------------*/
