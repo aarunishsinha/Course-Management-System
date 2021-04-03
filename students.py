33d339cbf7f837e0fdcaaba38c9f99a67819526d
Ashish Gehani <maniket3416@gmail.com>
2021-04-03 12:44:14 +0530

    I dont know what the fuck is happening *commits cat screams

--- students.py
+++ students.py
@@ -4,11 +4,28 @@ studentRoutes = Blueprint('studentRoutes',__name__)
 studentID = ""
 
 @studentRoutes.route("/studentScreen", methods = ["POST"])
-def inst():
+def std():
     global studentID
     studentID = request.form.get("StudentID")
     schedule = []
 
     # schedule = EXECUTE QUERY 6 HERE. Following order from slides
+
+    return render_template(student.html, studentID = studentID, schedule = schedule)
+
+@studentRoutes.route("/studentScreen/dropCourse", methods = ["POST"])
+def stdDropCourse():
+    global studentID
+    schedule = []
+    CN = request.form.get("CN")
+    COID = request.form.get("COID")
+    Days = request.form.get("Days")
+    STime = request.form.get("STime")
+    ETime = request.form.get("ETime")
+    Room = request.form.get("Room")
+    ST = request.form.get("ST")
+
+    # drop the course EXECUTE QUERY 3 here
+    # schedule = UPDATE Schedule. EXECUTE QUERY 6 HERE. Following order from slides
 
     return render_template(student.html, studentID = studentID, schedule = schedule)
