34411efeac4e52ac506df9393c6187726c4302c3
JaiJaveria <jaijaveria16@gmail.com>
2021-04-03 11:34:46 +0530

    Updated student query add course. Now it registers only when the schedule doesnt clash with any other registered courses of the student

--- admin.sql
+++ admin.sql
@@ -4,16 +4,25 @@ create or replace function start_addDrop(
 returns void as $$
 begin
 insert into addDrop values (tc);
+delete from pending_requests;
+delete from rejected_requests;
 end $$ language plpgsql;
+
+--EXAMPLE
+-- select * from start_addDrop(1214);
+/*-----------------------------------------------------------------------------*/
 
 create or replace function end_addDrop(
 	tc int)
 returns void as $$
 begin
 delete from addDrop where addDrop.term_code=(tc);
-delete from pending_requests;
-delete from rejected_requests;
+-- delete from pending_requests;
+-- delete from rejected_requests;
 end $$ language plpgsql;
+--EXAMPLE
+-- select * from end_addDrop(1214);
+/*-----------------------------------------------------------------------------*/
 
 create or replace function is_addDrop_on(
 	tc int)
@@ -21,19 +30,26 @@ returns boolean as $$
 begin
 return (select exists (select 1 from addDrop where addDrop.term_code=tc));
 end $$ language plpgsql;
-
---2 schedule and room allocation
-
--- create or replace function allocate_schedule_room(
--- 	tc int --term code
--- 	)
-
+--EXAMPLE
+-- select * from is_addDrop_on(1214);
 --3 create new course
+/*-----------------------------------------------------------------------------*/
+
 create or replace function addNewCourse(
 	code text,
 	name text)
 returns void as $$
 begin
-insert into courses values ('new2021' || code, name);
+	insert into courses values ('new2021' || code, name);
 end $$ language plpgsql;
+--EXAMPLE
+select * from addNewCourse('COMP_SCI774','Introduction to Data Mining');
+/*-----------------------------------------------------------------------------*/
 
+create or replace function addNewStudent(
+	id bigint,
+	name text)
+returns void as $$
+begin
+	insert into students values (id||name);
+end $$ language plpgsql;
--- generate_grade_distribution_percent.py
+++ generate_grade_distribution_percent.py
@@ -1,4 +1,12 @@
 #only used for generating some query in students.py
-x=['a_count','ab_count','b_count','bc_count','c_count','d_count','f_count','s_count','u_count','cr_count','n_count','p_count','i_count','nw_count','nr_count','other_count']
+# x=['a_count','ab_count','b_count','bc_count','c_count','d_count','f_count','s_count','u_count','cr_count','n_count','p_count','i_count','nw_count','nr_count','other_count']
+# for i in x:
+# 	print('(cast ('+i+' as float)/t.total*100)::numeric(10,2) as '+i+'_p,')
+
+#used for clash
+# x=['mon','tues','wed','thurs','fri','sat', 'sun']
+x=['start_time','end_time' ,'mon','tues','wed','thurs','fri','sat', 'sun']
+# x=[,end_time,mon,tues,wed,thurs,fri,sat,sun]
 for i in x:
-	print('(cast ('+i+' as float)/t.total*100)::numeric(10,2) as '+i+'_p,')
+	print(i+' as '+i+'_c,', end="")
+	# print('('+i+' and '+i+'_c) or ', end="")
