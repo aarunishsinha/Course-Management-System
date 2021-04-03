06fa0c7141849f16bd55fdbdfc74e11d4f68caef
Aarunish Sinha <58175652+aarunishsinha@users.noreply.github.com>
2021-04-03 21:43:44 +0530

    Student Queries linked

--- students.py
+++ students.py
@@ -123,17 +123,54 @@ def stdAddCourse():
     Sat = request.form.get("Sat")
     Sun = request.form.get("Sun")
     status = 10
+    check_reg = 0
+    addMsg="default"
     # status  = EXECUTE QUERY HERE
-    addMsg = "Invalid Request"
-    if status == 1:
-        addMsg = "Registered Successfully"
-    elif status == 0:
-        addMsg = "Registration Limit Exceeded. Request Pending"
-    elif status == -1:
-        addMsg = "Already Registered"
-    elif status == 2:
-        addMsg = "Course timings clash with schedule of already taken course"
-
+    try:
+        query1="""
+        BEGIN;
+        SELECT CASE WHEN exists(select * from rejected_requests where course_offering='%s' and student_id=%s) THEN 1 ELSE 0 END as checkRejected;
+        """ % (str(COID),str(studentID))
+        cur.execute(query1)
+        check_=cur.fetchall()
+        check_reg=check_[0][0]
+        cur.execute("COMMIT;")
+    except Exception as e:
+        print(e)
+    if check_reg==1:
+        addMsg="You are not allowed to add this course in the current term"
+    else:
+        try:
+            query2="""
+            BEGIN;
+            SELECT * from add_course(%s,%s,'%s');
+            """ % (str(studentID),str(SN),str(COID))
+            cur.execute(query2)
+            statu=cur.fetchall()
+            status=statu[0][0]
+            cur.execute("COMMIT;")
+        except Exception as e:
+            print(e)
+        addMsg = "Invalid Request"
+        if status == 1:
+            addMsg = "Registered Successfully"
+        elif status == 0:
+            addMsg = "Registration Limit Exceeded. Request Pending"
+        elif status == -1:
+            addMsg = "Already Registered"
+        elif status == 2:
+            addMsg = "Course timings clash with schedule of already taken course"
+    try:
+        query="""
+        BEGIN;
+        SELECT * from get_daily_schedule(%s);
+        """ % (str(studentID))
+        cur.execute(query)
+        schedule=cur.fetchall()
+        cur.execute("COMMIT;")
+    except Exception as e:
+        print (e)
+    print (addMsg)
     return render_template("student.html", studentID = studentID, schedule = schedule, pastStats = [], addMsg = addMsg, searchResults = [])
 
 
@@ -145,5 +182,15 @@ def stdPastStats():
     pastStats = [(0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10)]
 
     # pastStats = EXECUTE QUERY HERE
+    try:
+        query="""
+        BEGIN;
+        SELECT * from past_course_stats('%s');
+        """ % (str(CName))
+        cur.execute(query)
+        pastStats=cur.fetchall()
+        cur.execute("COMMIT;")
+    except Exception as e:
+        print (e)
 
     return render_template("student.html", studentID = studentID, schedule = schedule, pastStats = pastStats, addMsg = "", searchResults = [])
