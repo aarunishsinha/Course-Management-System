d62c61b44461ddd8c26b5b01ebeaabafce255ceb
Ashish Gehani <maniket3416@gmail.com>
2021-04-03 19:01:14 +0530

    Frontend done.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import psycopg2
 # import students
 from students import studentRoutes
+from Admin import adminRoutes
 # import nltk
 # import numpy as np
 # import pandas as pd
@@ -13,6 +14,7 @@ cur = conn.cursor()
 
 app = Flask(__name__)
 app.register_blueprint(studentRoutes)
+app.register_blueprint(adminRoutes)
 # app['debug'] = True
 
 # UPLOAD_FOLDER ='./uploads/'
--- students.py
+++ students.py
@@ -1,31 +1,103 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, Flask, render_template, send_from_directory, request, redirect, url_for
+import os
+import psycopg2
 
-studentRoutes = Blueprint('studentRoutes',__name__)
+
+studentRoutes = Blueprint('studentRoutes',__name__,template_folder='templates',static_folder='static')
 studentID = ""
+schedule = [("aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo"),("aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo"),("aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo")]
 
 @studentRoutes.route("/studentScreen", methods = ["POST"])
 def std():
     global studentID
+    global schedule
     studentID = request.form.get("StudentID")
-    schedule = []
-
     # schedule = EXECUTE QUERY 6 HERE. Following order from slides
 
-    return render_template(student.html, studentID = studentID, schedule = schedule)
+    return render_template("student.html", studentID = studentID, schedule = schedule, pastStats = [], addMsg = "", searchResults = [])
+
+@studentRoutes.route("/studentScreen/searchCourse", methods = ["POST"])
+def stdSearchCourse():
+    global studentID
+    global schedule
+    CName = request.form.get("CName")
+    TC = request.form.get("TC")
+    searchResults = [("aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo"),("aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo"),("aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo","aboo")]
+
+    # searchResults = EXECUTE QUERY 6 HERE. Following order from slides
+
+    return render_template("student.html", studentID = studentID, schedule = schedule, pastStats = [], addMsg = "", searchResults = searchResults)
+
 
 @studentRoutes.route("/studentScreen/dropCourse", methods = ["POST"])
 def stdDropCourse():
     global studentID
-    schedule = []
+    global schedule
+
     CN = request.form.get("CN")
-    COID = request.form.get("COID")
-    Days = request.form.get("Days")
+    SN = request.form.get("SN")
+    FC = request.form.get("FC")
+    Room = request.form.get("RC")
     STime = request.form.get("STime")
     ETime = request.form.get("ETime")
-    Room = request.form.get("Room")
-    ST = request.form.get("ST")
+    M = request.form.get("M")
+    T = request.form.get("T")
+    W = request.form.get("W")
+    Th = request.form.get("Th")
+    F = request.form.get("F")
+    Sat = request.form.get("Sat")
+    Sun = request.form.get("Sun")
+
+    schedule = [(1,1,1,1,1,1,1,1,1,1,1,1,1),(1,1,1,1,1,1,1,1,1,1,1,1,1)]
 
     # drop the course EXECUTE QUERY 3 here
     # schedule = UPDATE Schedule. EXECUTE QUERY 6 HERE. Following order from slides
 
-    return render_template(student.html, studentID = studentID, schedule = schedule)
+    return render_template("student.html", studentID = studentID, schedule = schedule, pastStats = [], addMsg = "", searchResults = [])
+
+@studentRoutes.route("/studentScreen/addCourse", methods = ["POST"])
+def stdAddCourse():
+    global studentID
+    global schedule
+    COID = request.form.get("COID")
+    CN = request.form.get("CN")
+    SN = request.form.get("SN")
+    CL = request.form.get("CL")
+    Inst = request.form.get("Inst")
+    DD = request.form.get("DD")
+    FC = request.form.get("FC")
+    Room = request.form.get("RC")
+    STime = request.form.get("STime")
+    ETime = request.form.get("ETime")
+    M = request.form.get("M")
+    T = request.form.get("T")
+    W = request.form.get("W")
+    Th = request.form.get("Th")
+    F = request.form.get("F")
+    Sat = request.form.get("Sat")
+    Sun = request.form.get("Sun")
+    status = 10
+    # status  = EXECUTE QUERY HERE
+    addMsg = "Invalid Request"
+    if status == 1:
+        addMsg = "Registered Successfully"
+    elif status == 0:
+        addMsg = "Registration Limit Exceeded. Request Pending"
+    elif status == -1:
+        addMsg = "Already Registered"
+    elif status == 2:
+        addMsg = "Course timings clash with schedule of already taken course"
+
+    return render_template("student.html", studentID = studentID, schedule = schedule, pastStats = [], addMsg = addMsg, searchResults = [])
+
+
+@studentRoutes.route("/studentScreen/pastStats", methods = ["POST"])
+def stdPastStats():
+    global studentID
+    global schedule
+    CName = request.form.get("CName")
+    pastStats = [(0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10,0.10)]
+
+    # pastStats = EXECUTE QUERY HERE
+
+    return render_template("student.html", studentID = studentID, schedule = schedule, pastStats = pastStats, addMsg = "", searchResults = [])
